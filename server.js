4a2527e698e148fbc3400a2e43c70f1a9dfb2076
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-10-04 14:05:13 +0800

    Health Check API Setup

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+
+/* App Config */
+const app = express();
+const port = process.env.PORT || 9000;
+
+/* Middlewares */
+
+/* Db Config */
+
+/* API Endpoints */
+app.get("/", (req, res) => {
+  res.status(200).send("Hello! This API Is Working!");
+});
+
+/* Listener */
+app.listen(port, () => console.log(`listening on localhost:${port}`));
