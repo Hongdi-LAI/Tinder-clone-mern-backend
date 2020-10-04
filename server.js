574a8457ff748c9c56ac6af0512b645504234bf2
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-10-04 14:16:10 +0800

    Database Schema Setup

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ import mongoose from "mongoose";
 /* App Config */
 const app = express();
 const port = process.env.PORT || 9000;
-
+const Db_URL =
+  "mongodb+srv://admin:Sj5y1Buixe6uz8eH@tinder-mern-database.auohm.mongodb.net/tinder-db?retryWrites=true&w=majority";
 /* Middlewares */
 
 /* Db Config */
-
+mongoose.connect(Db_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 /* API Endpoints */
 app.get("/", (req, res) => {
   res.status(200).send("Hello! This API Is Working!");
